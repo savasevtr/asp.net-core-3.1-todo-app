e7334d4a8e1d6ac26d3a3061d34b5546407a4b0c
savasevtr <savas.ev@yahoo.com>
2020-10-18 21:40:49 +0300

    update

---
+++ SEProje.ToDo.DataAccess/Concrete/EntityFrameworkCore/Context/TodoContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SEProje.ToDo.Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SEProje.ToDo.DataAccess.Concrete.EntityFrameworkCore.Context
+{
+    public class TodoContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            // optionsBuilder.UseSqlServer("server=.; database=UdemyTodo; integrated security=true;");
+            optionsBuilder.UseSqlServer("data source=localhost;Initial Catalog=UdemyTodo;Integrated Security=True");
+        }
+
+        public DbSet<Kullanici> Kullanicilar { get; set; }
+        public DbSet<Calisma> Calismalar { get; set; }
+    }
+}
