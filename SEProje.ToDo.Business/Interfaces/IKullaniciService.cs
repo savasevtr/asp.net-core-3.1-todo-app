4a761bbe3211f20981332f3c35a567579c658aa6
savasevtr <savas.ev@yahoo.com>
2020-10-20 21:38:09 +0300

    update

---
+++ SEProje.ToDo.Business/Interfaces/IKullaniciService.cs
@@ -0,0 +1,12 @@
+﻿using SEProje.ToDo.Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SEProje.ToDo.Business.Interfaces
+{
+    public interface IKullaniciService : IGenericService<Kullanici>
+    {
+
+    }
+}
