8d0d599a11b3c39bb50fe37113e2e6d8bfc26380
savasevtr <savas.ev@yahoo.com>
2020-10-21 22:43:14 +0300

    38. Libman Aracılığıyla Paket Yükleme ve wwwroot Klasörünün Dışarı Açılması

--- SEProje.ToDo.Web/Startup.cs
+++ SEProje.ToDo.Web/Startup.cs
@@ -27,6 +27,8 @@ namespace SEProje.ToDo.Web
                 app.UseDeveloperExceptionPage();
             }
 
+            app.UseStaticFiles();
+
             app.UseRouting();
 
             app.UseEndpoints(endpoints =>
