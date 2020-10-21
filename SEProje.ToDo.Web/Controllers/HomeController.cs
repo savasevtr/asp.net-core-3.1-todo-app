932c6eb168e47561ad9130daceb65eaace21bc39
savasevtr <savas.ev@yahoo.com>
2020-10-21 21:38:43 +0300

    update

--- SEProje.ToDo.Web/Controllers/HomeController.cs
+++ SEProje.ToDo.Web/Controllers/HomeController.cs
@@ -3,12 +3,29 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using SEProje.ToDo.Web.Models;
 
 namespace SEProje.ToDo.Web.Controllers
 {
     public class HomeController : Controller
     {
         public IActionResult Index()
+        {
+            ViewBag.isim = "Savaş";
+            TempData["isim"] = "Ramo";
+            ViewData["isim"] = "Deli Veli";
+
+            return View(new List<MusteriViewModel>
+            {
+                new MusteriViewModel() { Ad = "Ali" },
+                new MusteriViewModel() { Ad = "Ramo" },
+                new MusteriViewModel() { Ad = "Cemo" },
+                new MusteriViewModel() { Ad = "Memo" },
+                new MusteriViewModel() { Ad = "Ayşo" },
+            });
+        }
+
+        public IActionResult Sonuc()
         {
             return View();
         }
---
+++ SEProje.ToDo.Web/Models/MusteriViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SEProje.ToDo.Web.Models
+{
+    public class MusteriViewModel
+    {
+        public string Ad { get; set; }
+    }
+}
