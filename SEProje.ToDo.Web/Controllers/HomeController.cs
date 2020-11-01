45e2b8ca29385d3e48fe7599f4678b97a73a4985
savasevtr <savas.ev@yahoo.com>
2020-11-01 09:53:50 +0300

    kullanıcı kayıt işlemi

--- SEProje.ToDo.Web/Controllers/HomeController.cs
+++ SEProje.ToDo.Web/Controllers/HomeController.cs
@@ -1,10 +1,20 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using SEProje.ToDo.Entities.Concrete;
 using SEProje.ToDo.Web.Models;
+using System.Threading.Tasks;
 
 namespace SEProje.ToDo.Web.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly UserManager<AppUser> _userManager;
+
+        public HomeController(UserManager<AppUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
         public IActionResult Index()
         {
             return View();
@@ -16,14 +26,49 @@ namespace SEProje.ToDo.Web.Controllers
         }
 
         [HttpPost]
-        public IActionResult KayitOl(AppUserAddViewModel model)
+        public async Task<IActionResult> KayitOl(AppUserAddViewModel model)
         {
             if (ModelState.IsValid)
             {
+                AppUser user = new AppUser()
+                {
+                    UserName = model.UserName,
+                    Email = model.Email,
+                    Name = model.Name,
+                    Surname = model.Surname
+                };
 
+                var result = await _userManager.CreateAsync(user, model.Password);
+
+                if (result.Succeeded)
+                {
+                    var addRoleResult = await _userManager.AddToRoleAsync(user, "Member");
+
+                    if (addRoleResult.Succeeded)
+                    {
+                        return RedirectToAction("GirisYap");
+                    }
+
+                    foreach (var item in addRoleResult.Errors)
+                    {
+                        ModelState.AddModelError("", item.Description);
+                    }
+                }
+
+                foreach (var item in result.Errors)
+                {
+                    ModelState.AddModelError("", item.Description);
+                }
             }
 
             return View(model);
         }
+
+        public IActionResult GirisYap()
+        {
+            return View();
+        }
+
+
     }
 }
