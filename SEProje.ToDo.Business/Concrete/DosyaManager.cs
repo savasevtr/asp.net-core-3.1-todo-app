b2e95568c4f7add15da10f2f9c468a59209afa8e
savasevtr <savas.ev@yahoo.com>
2020-11-05 22:00:44 +0300

    DosyaManager & IDosyaManager

---
+++ SEProje.ToDo.Business/Concrete/DosyaManager.cs
@@ -0,0 +1,63 @@
+﻿using FastMember;
+using iTextSharp.text;
+using iTextSharp.text.pdf;
+using OfficeOpenXml;
+using SEProje.ToDo.Business.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.IO;
+using System.Net.Http.Headers;
+
+namespace SEProje.ToDo.Business.Concrete
+{
+    public class DosyaManager : IDosyaManager
+    {
+        public byte[] AktarExcel<T>(List<T> entity) where T : class, new()
+        {
+            var excelPackage = new ExcelPackage();
+            var excelBlank = excelPackage.Workbook.Worksheets.Add("Calisma1");
+
+            excelBlank.Cells["A1"].LoadFromCollection(entity, true, OfficeOpenXml.Table.TableStyles.Light15);
+
+            return excelPackage.GetAsByteArray();
+        }
+
+        public string AktarPdf<T>(List<T> entity) where T : class, new()
+        {
+            DataTable dataTable = new DataTable();
+            dataTable.Load(ObjectReader.Create(entity));
+
+            Document document = new Document(PageSize.A4, 25f, 25f, 25f, 25f);
+
+            var fileName = Guid.NewGuid() + ".pdf";
+            var returnPath = "/documents/" + fileName;
+            var path = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/documents/" + fileName);
+
+            var stream = new FileStream(path, FileMode.Create);
+
+            PdfWriter.GetInstance(document, stream);
+
+            document.Open();
+            PdfPTable pdfPTable = new PdfPTable(dataTable.Columns.Count);
+
+            for (int i = 0; i < dataTable.Columns.Count; i++)
+            {
+                pdfPTable.AddCell(dataTable.Columns[i].ColumnName);
+            }
+
+            for (int i = 0; i < dataTable.Rows.Count; i++)
+            {
+                for (int j = 0; j < dataTable.Columns.Count; j++)
+                {
+                    pdfPTable.AddCell(dataTable.Rows[i][j].ToString());
+                }
+            }
+
+            document.Add(pdfPTable);
+            document.Close();
+
+            return returnPath;
+        }
+    }
+}
---
+++ SEProje.ToDo.Business/Interfaces/IDosyaManager.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace SEProje.ToDo.Business.Interfaces
+{
+    public interface IDosyaManager
+    {
+        string AktarPdf<T>(List<T> entity) where T : class, new();
+        byte AktarExcel<T>(List<T> entity) where T : class, new();
+    }
+}
