98300e31aa32b126cc3f47549a70127d2c8fbdb7
savasevtr <savas.ev@yahoo.com>
2020-11-14 12:44:35 +0300

    Fluent Validation

---
+++ SEProje.ToDo.Business/ValidationRules/FluentValidation/AciliyetAddValidator.cs
@@ -0,0 +1,13 @@
+﻿using FluentValidation;
+using SEProje.ToDo.DTO.DTOs.AciliyetDTOs;
+
+namespace SEProje.ToDo.Business.ValidationRules.FluentValidation
+{
+    public class AciliyetAddValidator : AbstractValidator<AciliyetAddDto>
+    {
+        public AciliyetAddValidator()
+        {
+            RuleFor(I => I.Tanim).NotNull().WithMessage("Tanım alanı boş geçilemez");
+        }
+    }
+}
---
+++ SEProje.ToDo.Business/ValidationRules/FluentValidation/AciliyetUpdateValidator.cs
@@ -0,0 +1,13 @@
+﻿using FluentValidation;
+using SEProje.ToDo.DTO.DTOs.AciliyetDTOs;
+
+namespace SEProje.ToDo.Business.ValidationRules.FluentValidation
+{
+    public class AciliyetUpdateValidator : AbstractValidator<AciliyetUpdateDto>
+    {
+        public AciliyetUpdateValidator()
+        {
+            RuleFor(I => I.Tanim).NotNull().WithMessage("Tanım alanı boş geçilemez!");
+        }
+    }
+}
