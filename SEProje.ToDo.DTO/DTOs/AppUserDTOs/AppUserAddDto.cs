50e3f2257b68f4cfd8716c8e34570bf8c39e73cd
savasevtr <savas.ev@yahoo.com>
2020-11-21 12:00:09 +0300

    update

--- SEProje.ToDo.DTO/DTOs/AppUserDTOs/AppUserAddDto.cs
+++ SEProje.ToDo.DTO/DTOs/AppUserDTOs/AppUserAddDto.cs
@@ -8,9 +8,11 @@ namespace SEProje.ToDo.DTO.DTOs.AppUserDTOs
         public string UserName { get; set; }
 
         [Display(Name = "Şifre :")]
+        [DataType(DataType.Password)]
         public string Password { get; set; }
 
         [Display(Name = "Şifre Tekrar :")]
+        [DataType(DataType.Password)]
         public string ConfirmPassword { get; set; }
 
         [Display(Name = "Email")]
--- SEProje.ToDo.Web/ViewComponents/Wrapper.cs
+++ SEProje.ToDo.Web/ViewComponents/Wrapper.cs
@@ -1,6 +1,8 @@
-﻿using Microsoft.AspNetCore.Identity;
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using SEProje.ToDo.Business.Interfaces;
+using SEProje.ToDo.DTO.DTOs.AppUserDTOs;
 using SEProje.ToDo.Entities.Concrete;
 using SEProje.ToDo.Web.Areas.Admin.Models;
 
@@ -10,31 +12,26 @@ namespace SEProje.ToDo.Web.ViewComponents
     {
         private readonly UserManager<AppUser> _userManager;
         private readonly IBildirimService _bildirimService;
+        private readonly IMapper _mapper;
 
-        public Wrapper(UserManager<AppUser> userManager, IBildirimService bildirimService)
+        public Wrapper(UserManager<AppUser> userManager, IBildirimService bildirimService, IMapper mapper)
         {
             _userManager = userManager;
             _bildirimService = bildirimService;
+            _mapper = mapper;
         }
 
         public IViewComponentResult Invoke()
         {
-            var user = _userManager.FindByNameAsync(User.Identity.Name).Result;
+            var identityUser = _userManager.FindByNameAsync(User.Identity.Name).Result;
 
-            AppUserListViewModel model = new AppUserListViewModel
-            {
-                Id = user.Id,
-                Name = user.Name,
-                Surname = user.Surname,
-                Picture = user.Picture,
-                Email = user.Email
-            };
+            var model = _mapper.Map<AppUserListDto>(identityUser);
 
-            var bildirimler = _bildirimService.GetirOkunmayanlar(user.Id).Count;
+            var bildirimler = _bildirimService.GetirOkunmayanlar(model.Id).Count;
 
             ViewBag.bildirimSayisi = bildirimler;
 
-            var roles = _userManager.GetRolesAsync(user).Result;
+            var roles = _userManager.GetRolesAsync(identityUser).Result;
 
             if (roles.Contains("Admin"))
             {
